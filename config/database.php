acc837d0d021c586dbdd0d28a8a5c020f5530500
hendralatif <ahmadlatifhendrawan@gmail.com>
2026-07-14 05:25:22 +0700

    fix: fix SSL config for Aiven MySQL (SSL REQUIRED mode)

--- config/database.php
+++ config/database.php
@@ -58,10 +58,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            'options' => extension_loaded('pdo_mysql') ? [
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', null),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_CIPHER => 'HIGH:!SSLv2:!SSLv3',
+            ] : [],
         ],
 
         'mariadb' => [
